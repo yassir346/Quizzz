9f761fe1d49f31a1597bd781babdd229ea087f3e
yassir346 <yassir.mahir98@gmail.com>
2024-12-30 16:37:16 +0100

    fonction de showResults

--- quiz.js
+++ quiz.js
@@ -44,3 +44,37 @@ function passQuestions(){
         }
 
 
+        function showResults(){
+
+     
+            const answerContainers = quizContainer.querySelectorAll('.answers');
+          
+
+            let numCorrect = 0;
+          
+     
+            questions.forEach( (currentQuestion, questionNumber) => {
+          
+        
+              const answerContainer = answerContainers[questionNumber];
+              const selector = `input[name=question${questionNumber}]:checked`;
+              const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+          
+
+              if(userAnswer === currentQuestion.correctAnswer){
+    
+                numCorrect++;
+          
+
+                answerContainers[questionNumber].style.color = 'lightgreen';
+              }
+
+              else{
+
+                answerContainers[questionNumber].style.color = 'red';
+              }
+            });
+
+            resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
+          }
+
