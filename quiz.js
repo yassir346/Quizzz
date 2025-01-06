7592a85ea50467a9c2fd8a3cde49c31c8f710a9f
yassir346 <yassir.mahir98@gmail.com>
2025-01-06 13:36:58 +0100

    doing modifications

--- quiz.js
+++ quiz.js
@@ -16,65 +16,110 @@ const trueBTN = document.getElementById('true-btn');
 const falseBTN = document.getElementById('false-btn');
 const score = document.getElementById('score');
 
+let index = 0;
+let Score = 0;
 
-function passQuestions(){
-    const output = [];
+function questionFiller(){
+  Question.innerHTML = questions[index++].question;
+};
 
-    questions.forEach(
-        (currentQuestion, questionNumber) => {
-
-            const answers = [];
-            for(letter in questions.answer){
-
-                answers.push(
-                  `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${questions.answer[letter]}
-                  </label>`
-                );
-              }
-                 output.push(
-                `<div class="question"> ${questions.question} </div>
-                <div class="answers"> ${answer.join('')} </div>`
-              );
-            }
-         )
-         Question.innerHTML = output.join('');
-        }
+questionFiller();
 
 
-        function showResults(){
+
+trueBTN.addEventListener("click", function(){
+
+  if(questions[index].answer == trueBTN.innerHTML){
+    Score = Score++;
+  }
+
+  if(index <= questions.length){
+  Question.innerHTML = questions[index++].question;
+}
+
+else{
+  Question.innerHTML = `"Vous avez terminez le Quiz avec le score de" ${Score}`;
+}
+
+})
+
+
+
+falseBTN.addEventListener("click", function(){
+
+  if(questions[index].answer == trueBTN.innerHTML){
+    Score = Score;
+  }
+
+  if(index <= questions.length){
+  Question.innerHTML = questions[index++].question;
+}
+
+else{
+  Question.innerHTML = "Vous avez terminez le Quiz avec le score de ${Score} " ;
+}
+
+
+})
+
+// function passQuestions(){
+//     const output = [];
+
+//     questions.forEach(
+//         (currentQuestion, questionNumber) => {
+
+//             const answers = [];
+//             for(letter in questions.answer){
+
+//                 answers.push(
+//                   `<label>
+//                     <input type="radio" name="question${questionNumber}" value="${letter}">
+//                     ${letter} :
+//                     ${questions.answer[letter]}
+//                   </label>`
+//                 );
+//               }
+//                  output.push(
+//                 `<div class="question"> ${questions.question} </div>
+//                 <div class="answers"> ${answer.join('')} </div>`
+//               );
+//             }
+//          )
+//          Question.innerHTML = output.join('');
+//         }
+
+
+// function showResults(){
 
      
-            const answerContainers = quizContainer.querySelectorAll('.answers');
+//         const answerContainers = quizContainer.querySelectorAll('.answers');
           
 
-            let numCorrect = 0;
+//         let numCorrect = 0;
           
      
-            questions.forEach( (currentQuestion, questionNumber) => {
+//         questions.forEach( (currentQuestion, questionNumber) => {
           
         
-              const answerContainer = answerContainers[questionNumber];
-              const selector = `input[name=question${questionNumber}]:checked`;
-              const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+//               const answerContainer = answerContainers[questionNumber];
+//               const selector = `input[name=question${questionNumber}]:checked`;
+//               const userAnswer = (answerContainer.querySelector(selector) || {}).value;
           
 
-              if(userAnswer === currentQuestion.correctAnswer){
+//               if(userAnswer === currentQuestion.correctAnswer){
     
-                numCorrect++;
+//                 numCorrect++;
           
 
-                answerContainers[questionNumber].style.color = 'lightgreen';
-              }
+//                 answerContainers[questionNumber].style.color = 'lightgreen';
+//               }
 
-              else{
+//               else{
 
-                answerContainers[questionNumber].style.color = 'red';
-              }
-            });
+//                 answerContainers[questionNumber].style.color = 'red';
+//               }
+//             });
 
-            resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
-          }
+//             resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
+//           }
 
